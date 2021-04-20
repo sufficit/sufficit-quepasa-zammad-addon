fd2dc25c747ee8af1ba2c30ed7fef2b3ae35733b
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2021-04-20 00:09:06 -0300

    migrations;

---
+++ src/db/addon/quepasa/20210420025706_quepasa_add_whatsapp_uniqueid_to_users.rb
@@ -0,0 +1,6 @@
+class QuepasaAddWhatsAppUniqueIDToUsers < ActiveRecord::Migration[5.2]
+  def change    
+    #Incluindo campo para o ID do WhatsApp
+    add_column :users, :whatsapp_uniqueid, :string
+  end
+end
