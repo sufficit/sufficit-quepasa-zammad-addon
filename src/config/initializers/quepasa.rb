af62aab22491664929b1441fb17fd96a80897b76
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-01-03 13:00:51 -0300

    remove comments

--- src/config/initializers/quepasa.rb
+++ src/config/initializers/quepasa.rb
@@ -3,15 +3,14 @@ Rails.application.config.before_configuration do
     doc = File.open("public/assets/images/icons.svg") { |f| Nokogiri::XML(f) }
     if !doc.at_css('#icon-quepasa')
         doc.at('svg').add_child(icon)
-        Rails.logger.info "quepasa icon added to icon set"
-    else
-        Rails.logger.info "quepasa icon already in icon set"
+        Rails.logger.info "Quepasa icon added to icon set."
+    #else # dbug porpouses
+    #    Rails.logger.info "Quepasa icon already in icon set."
     end
     #File.write("public/assets/images/icons.svg", doc.to_xml)
 end
   
 Rails.application.config.after_initialize do
     Ticket::Article.include(Ticket::Article::EnqueueCommunicateQuepasaJob)
-    #Ticket::Article.add EnqueueCommunicateQuepasaJob.instance
-    #Ticket::Article.add_observer Observer::Ticket::Article::CommunicateQuepasa.instance
+    Rails.logger.info "Quepasa enqueue communicate jobs."
 end
